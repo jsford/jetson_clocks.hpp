56313ff6adbc5c3f60b99134128df371e30c815f
Jordan Ford <jsford94@gmail.com>
2019-09-17 09:12:02 -0400

    Not trying to duplicate the jetson_clocks.sh script. Backing off to just provide power state manipulation utilities.

--- jetson_clocks.hpp
+++ jetson_clocks.hpp
@@ -2,19 +2,13 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 #include <unistd.h>
 
 namespace jetson_clocks {
 
 static const std::string ENV_HOME = std::getenv("HOME");
-
-static const std::string RED="\e[0;31m";
-static const std::string GREEN="\e[0;32m";
-static const std::string BLUE="\e[0;34m";
-static const std::string BRED="\e[1;31m";
-static const std::string BGREEN="\e[1;32m";
-static const std::string BBLUE="\e[1;34m";
-static const std::string NC="\e[0m";
+static const std::string DEFAULT_CONF_FILE = ENV_HOME + "/l4t_dfs.conf";
 
 bool running_as_root() {
     return (geteuid() == 0);
@@ -25,19 +19,31 @@ bool file_exists(const std::string& name) {
     return f.good();
 }
 
-std::string cat_file(const std::string& name) {
+bool file_writable(const std::string& name) {
+    FILE *fp = fopen(name.c_str(), "w");
+    return fp != NULL;
+}
+
+std::string read_file(const std::string& name) {
     std::ifstream t(name.c_str());
     std::stringstream buffer;
     buffer << t.rdbuf();
     return buffer.str();
 }
 
+bool write_file(const std::string& name, const std::string& str) {
+    if(!file_writable(name)) { return false; }
+    std::ofstream out(name.c_str());
+    out << str;
+    return true;
+}
+
 std::string get_soc_family() {
     std::string soc_family = "";
     if(file_exists("/sys/devices/soc0/family")) {
-        soc_family = cat_file("/sys/devices/soc0/family");
+        soc_family = read_file("/sys/devices/soc0/family");
     } else if(file_exists("/proc/device-tree/compatible")) {
-       std::string compat_file = cat_file("/proc/device-tree/compatible");
+       std::string compat_file = read_file("/proc/device-tree/compatible");
        if(compat_file.find("nvidia,tegra210") != std::string::npos) {
         soc_family = "tegra210";
        } else if(compat_file.find("nvidia,tegra186") != std::string::npos) {
@@ -51,22 +57,28 @@ std::string get_machine() {
     std::string machine = "";
     if(file_exists("/sys/devices/soc0/family")) {
         if(file_exists("/sys/devices/soc0/machine")) {
-            machine = cat_file("/sys/devices/soc0/machine");
+            machine = read_file("/sys/devices/soc0/machine");
         }
     } else if(file_exists("/proc/device-tree/model")) {
-        machine = cat_file("/proc/device-tree/model");
+        machine = read_file("/proc/device-tree/model");
     }
     return machine;
 }
 
-bool store(const std::string& conf_file) {}
-bool restore(const std::string& conf_file) {}
+bool set_fan_speed(unsigned char speed) {
+    // Jetson-TK1 CPU fan is always ON.
+    if(get_machine() == "jetson-tk1") {
+        return true;
+    }
 
-bool do_fan() {}
-bool do_clusterswitch() {}
-bool do_hotplug() {}
-bool do_cpu() {}
-bool do_gpu() {}
-bool do_emc() {}
+    if(!file_writable("/sys/kernel/debug/tegra_fan/target_pwm")) {
+        std::cout << "Can't access Fan!" << std::endl;
+        return false;
+    }
+
+    std::string speed_string = std::to_string(speed);
+    return write_file("/sys/kernel/debug/tegra_fan/target_pwm", speed_string);
+}
+
 
 }   // namespace jetson_clocks
